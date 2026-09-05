1cc59ddf2c74819e25dab2d28a79aca546a8d1e2
SYSTEMTOOLBOX <cncsystem.mec@gmail.com>
2026-09-05 08:15:18 +0200

    Bust Satispay linked UI cache v6

--- scripts/apply_community_ui_v2.py
+++ scripts/apply_community_ui_v2.py
@@ -17,7 +17,7 @@ if not profile_match:
 ui_tag='<script src="./community-ui-v2.js?v=9"></script>'
 fix_tag='<script src="./community-document-ui-fix.js?v=2"></script>'
 qr_tag='<script src="./community-qr.js?v=2"></script>'
-account_tag='<script src="./community-phone-verification.js?v=5"></script>'
+account_tag='<script src="./community-phone-verification.js?v=6"></script>'
 
 if re.search(ui_pattern,s):
     s=re.sub(ui_pattern,ui_tag,s,count=1)
